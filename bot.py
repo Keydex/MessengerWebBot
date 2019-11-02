df59e592bf8828b23c9a6897845c042d71ff461a
Anthony Pham <anthony.pham@syngenta.com>
2019-11-02 13:04:39 -0500

    Added state management

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request
 from pymessenger.bot import Bot
+import json
 
 
 load_dotenv()
@@ -12,31 +13,83 @@ ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 bot = Bot(ACCESS_TOKEN)
 
+
+# Store Conversation State here
+userState = {}
+
+class user:
+    def __init__(self, userID):
+        self.userID = userID
+        self.testvar = 'test'
+        self.currState = 'initial'
+        self.websites = []
+
+    def updateState(self, newState):
+        self.currState = newState
+
+    # def websites():
+
+class website:
+    def __init__(self):
+        self.websiteName = ''
+        self.websiteDescription = ''
+        self.productList = []
+
+class product:
+    def __init__(self, name, price = None, imageLink = None, productCategory = None):
+        self.name = name
+        self.price = price
+        self.imageLink = imageLink
+        self.productCategory = ''
+
+def createUser(userID):
+    userState[userID] = user
+    print('Create new user!')
+
+def sendInitialMessage(userID):
+    response = 'This should be the first message I send you back!'
+    bot.send_text_message(userID, response)
+
+def resetState():
+    userState.clear()
+
 #We will receive messages that Facebook sends our bot at this endpoint 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=['GET'])
+def verify_message():
+    """Before allowing people to message your bot, Facebook has implemented a verify token
+    that confirms all requests that your bot receives came from Facebook.""" 
+    token_sent = request.args.get("hub.verify_token")
+    return verify_fb_token(token_sent)
+
+#We will receive messages that Facebook sends our bot at this endpoint 
+@app.route("/", methods=['POST'])
 def receive_message():
-    if request.method == 'GET':
-        """Before allowing people to message your bot, Facebook has implemented a verify token
-        that confirms all requests that your bot receives came from Facebook.""" 
-        token_sent = request.args.get("hub.verify_token")
-        return verify_fb_token(token_sent)
-    #if the request was not get, it must be POST and we can just proceed with sending a message back to user
-    else:
-        # get whatever message a user sent the bot
-       output = request.get_json()
-       for event in output['entry']:
-          messaging = event['messaging']
-          for message in messaging:
+    print('Received Message')
+    # get whatever message a user sent the bot
+    output = request.get_json()
+    for event in output['entry']:
+        if (event['messaging'] is None):
+            print('No messages in event')
+            return "Message Processed"
+        messaging = event['messaging']
+        for message in messaging:
+        # Check if user exists in state management, if not create user
             if message.get('message'):
-                #Facebook Messenger ID for user so we know where to send response back to
                 recipient_id = message['sender']['id']
-                if message['message'].get('text'):
-                    response_sent_text = get_message()
-                    send_message(recipient_id, response_sent_text)
-                #if user sends us a GIF, photo,video, or any other non-text item
-                if message['message'].get('attachments'):
-                    response_sent_nontext = get_message()
-                    send_message(recipient_id, response_sent_nontext)
+                print("Processing Message")
+                if message['message'].get('text') == 'debugReset':
+                    resetState()
+                    send_message(recipient_id, 'Debug: Your State should now be reset!')
+                    print('Resetting State')
+                    return "Message Processed"
+                if (recipient_id not in userState):
+                    createUser(recipient_id)
+                    sendInitialMessage(recipient_id)
+                    print('Sending initial Message')
+                else: 
+                    if message.get('message'):
+                        print(message['message'].get('text'))
+                        send_message(recipient_id, 'This should be the same message now')
     return "Message Processed"
 
 
@@ -46,7 +99,7 @@ def verify_fb_token(token_sent):
     if token_sent == VERIFY_TOKEN:
         return request.args.get("hub.challenge")
     print(token_sent)
-    return 'Invalid verification token'
+    return 'Invalid verification token, you shouldn\'t be able to access this page'
 
 
 #chooses a random message to send to the user
